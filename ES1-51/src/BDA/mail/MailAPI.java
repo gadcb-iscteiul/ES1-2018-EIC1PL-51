73dbb9655c5578dff789e3b80e790971ccff7db4
Marcos Silva <44324057+mpcts-iscteiul@users.noreply.github.com>
2018-12-06 22:51:30 +0000

    Update MailAPI.java
    Class Mail

--- ES1-51/src/BDA/mail/MailAPI.java
+++ ES1-51/src/BDA/mail/MailAPI.java
@@ -4,12 +4,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.mail.*;
-
+import javax.mail.internet.*;
+import java.util.*;
 import BDA.main.GUI_API;
 
 public class MailAPI {
 
 	private GUI_API gui;
+	final String senderEmailID = "rishabhraj210@gmail.com";
+	final String senderPassword = "7277077256";
+	final String emailSMTPserver = "smtp.gmail.com";
+	final String emailServerPort = "465";
+	final String receiverEmailID = "rishabhraj210@gmail.com";
+	static String emailSubject = "Test Mail";
+	static String emailBody = ":)";
 
 	public MailAPI(GUI_API gui) {
 		this.gui = gui;
@@ -17,25 +25,110 @@ public class MailAPI {
 		start();
 	}
 
-	private void start() {//JavaXMailLibery
+	private void start() {
+		//1 Abrir email definido
+		//2 Ler email
+		//3 imprimir para interface grafica.
 	}
 
-
+	private void SendEmail(String receiverEmailID, String Subject, String Body) {
+		// Receiver Email Address
+	}
 
 	private void addButtonActions() {
 		gui.getButton_mail().addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				send();
+				//send();
 			}
 
-			private void send() {
-				//String mail = (String) gui.getList_mail().getSelectedValue();// para obter o Mail selecionado.
-			}
+			private void send(String receiverEmailID, String Subject, String Body) {
+				// String mail = (String)
+				// gui.getList_mail().getSelectedValue();// para obter o
+				// Mail selecionado. para enviar.
+				
+				//O MAIL JÁ EXISTE, É SÓ REECAMINHAR O MAIL.
+				
+				//this.receiverEmailID = receiverEmailID;
+				// Subject
+				//this.emailSubject = Subject;
+				// Body
+				//this.emailBody = Body;
+				
+				Properties props = new Properties();
+				props.put("mail.smtp.user", senderEmailID);
+				props.put("mail.smtp.host", emailSMTPserver);
+				props.put("mail.smtp.port", emailServerPort);
+				props.put("mail.smtp.starttls.enable", "true");
+				props.put("mail.smtp.auth", "true");
+				props.put("mail.smtp.socketFactory.port", emailServerPort);
+				props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+				props.put("mail.smtp.socketFactory.fallback", "false");
+				SecurityManager security = System.getSecurityManager();
+				try {
+					Authenticator auth = new SMTPAuthenticator();
+					Session session = Session.getInstance(props, auth);
+					MimeMessage msg = new MimeMessage(session);
+					msg.setText(emailBody);
+					msg.setSubject(emailSubject);
+					msg.setFrom(new InternetAddress(senderEmailID));
+					msg.addRecipient(Message.RecipientType.TO, new InternetAddress(receiverEmailID));
+					Transport.send(msg);
+					System.out.println("Message send Successfully:)");
+				}
 
+				catch (Exception mex) {
+					mex.printStackTrace();
+				}
+			}
 		});
+	}
+	
+	public class SMTPAuthenticator extends javax.mail.Authenticator {
+		public PasswordAuthentication getPasswordAuthentication() {
+			return new PasswordAuthentication(senderEmailID, senderPassword);
+		}
 
 	}
-
 }
+
+/*
+ *	codigo a implementar..
+ * import javax.mail.*; 
+ * import javax.mail.internet.*; 
+ * import java.util.*;
+ * 
+ * private void SendEmail {
+ * 
+ * final String senderEmailID = "rishabhraj210@gmail.com"; final String
+ * senderPassword = "7277077256"; final String emailSMTPserver =
+ * "smtp.gmail.com"; final String emailServerPort = "465"; String
+ * receiverEmailID = null; static String emailSubject = "Test Mail"; static
+ * String emailBody = ":)"; public void SendEmail(String receiverEmailID,String
+ * Subject, String Body){
+ * 
+ * // Receiver Email Address this.receiverEmailID=receiverEmailID; // Subject
+ * this.emailSubject=Subject; // Body this.emailBody=Body; Properties props =
+ * new Properties(); props.put("mail.smtp.user",senderEmailID);
+ * props.put("mail.smtp.host", emailSMTPserver); props.put("mail.smtp.port",
+ * emailServerPort); props.put("mail.smtp.starttls.enable", "true");
+ * props.put("mail.smtp.auth", "true");
+ * props.put("mail.smtp.socketFactory.port", emailServerPort);
+ * props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+ * props.put("mail.smtp.socketFactory.fallback", "false"); SecurityManager
+ * security = System.getSecurityManager(); try{ Authenticator auth = new
+ * SMTPAuthenticator(); Session session = Session.getInstance(props, auth);
+ * MimeMessage msg = new MimeMessage(session); msg.setText(emailBody);
+ * msg.setSubject(emailSubject); msg.setFrom(new
+ * InternetAddress(senderEmailID)); msg.addRecipient(Message.RecipientType.TO,
+ * new InternetAddress(receiverEmailID)); Transport.send(msg);
+ * System.out.println("Message send Successfully:)"); }
+ * 
+ * catch (Exception mex){ mex.printStackTrace();}
+ * 
+ * 
+ * } public class SMTPAuthenticator extends javax.mail.Authenticator { public
+ * PasswordAuthentication getPasswordAuthentication() { return new
+ * PasswordAuthentication(senderEmailID, senderPassword); } } }
+ */
